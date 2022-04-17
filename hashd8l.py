2f6f003dea1a40419912b980fe6fb613d189ac5f
d8l6lsiz <103894024+d8l6lsiz@users.noreply.github.com>
2022-04-17 18:58:08 +0300

    Create hashd8l.py

---
+++ hashd8l.py
@@ -0,0 +1,63 @@
+import hashlib
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+print(Fore.BLUE,"██████   ██████   ██████  ████████  ██████  ██████   █████  ██               ██████  ██      ███████ ██ ███████")
+print(Fore.BLUE,"██   ██ ██    ██ ██    ██    ██    ██    ██ ██   ██ ██   ██ ██              ██       ██      ██      ██    ███")
+print(Fore.BLUE,"██████  ██    ██ ██    ██    ██    ██ ██ ██ ██   ██  █████  ██              ███████  ██      ███████ ██   ███")
+print(Fore.BLUE,"██   ██ ██    ██ ██    ██    ██    ██ ██ ██ ██   ██ ██   ██ ██              ██    ██ ██           ██ ██  ███")
+print(Fore.BLUE,"██   ██  ██████   ██████     ██     █ ████  ██████   █████  ███████ ███████  ██████  ███████ ███████ ██ ███████")
+print("                                                                                                                         ")
+
+print(Fore.BLUE,"[1] MD5")
+print(Fore.BLUE,"[2] SHA256")
+
+sec = input("Choose: ")
+if sec == "1":
+
+    def tryOpen(wordlist):
+        global pass_file
+        try:
+            pass_file = open(wordlist, "r")
+        except:
+            print(Fore.RED, "file location is wrong")
+            quit()
+
+
+    crackhash = input("Hash: ")
+    wordlist = input("Wordlist: ")
+    tryOpen(wordlist)
+    for word in pass_file:
+        print(Fore.CYAN, "[+] Trying: " + word.strip("\n"))
+        enc_word = word.encode("utf-8")
+        md5Digest = hashlib.md5(enc_word.strip()).hexdigest()
+        if md5Digest == crackhash:
+            print(Fore.GREEN, f"Hash cracked: {word} ")
+            exit()
+    print(Fore.YELLOW, "password not found on file")
+
+if sec == "2":
+
+    def tryOpen(wordlist):
+        global pass_file
+        try:
+            pass_file = open(wordlist, "r")
+        except:
+            print(Fore.RED, "file location is wrong")
+            quit()
+
+
+    crackhash = input("Hash: ")
+    wordlist = input("Wordlist: ")
+    tryOpen(wordlist)
+    for word in pass_file:
+        print(Fore.CYAN, "[+] Trying: " + word.strip("\n"))
+        enc_word = word.encode("utf-8")
+        sha256Digest = hashlib.sha256(enc_word.strip()).hexdigest()
+        if sha256Digest == crackhash:
+            print(Fore.GREEN, f"Hash cracked: {word} ")
+            exit()
+    print(Fore.YELLOW, "password not found on file")
+
